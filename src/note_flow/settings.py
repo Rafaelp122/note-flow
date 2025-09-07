8c4a7f938b3a7b78e8d7b7a526ab6ca0daf26294
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-09-07 13:53:27 -0300

    feat(templates): add base template and update home template to extend it

--- src/note_flow/settings.py
+++ src/note_flow/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'note_flow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
