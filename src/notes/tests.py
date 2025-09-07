fd150d72b26c363991839e2d5b974a47971afd9f
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-09-07 12:13:48 -0300

    test(notes): add unit tests for notes home view and URL routing

--- src/notes/tests.py
+++ src/notes/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from notes import views
 
-# Create your tests here.
+
+class NoteTests(TestCase):
+    def test_notes_home_url_is_correct(self):
+        url = reverse("notes:home")
+        self.assertEqual(url, "/")
+
+    def test_notes_home_view_function_is_correct(self):
+        view = resolve(reverse("notes:home"))
+        self.assertIs(view.func, views.home)
+
+    def test_notes_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse("notes:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse("notes:home"))
+        self.assertTemplateUsed(response, "notes/home.html")
