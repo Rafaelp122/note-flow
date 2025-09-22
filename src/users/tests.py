44afe035c57ac9d351077436053859505ab5a3c9
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-09-21 23:24:15 -0300

    feat(users): add sign-in and sign-up views, templates, and tests

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class UsersViewsTests(TestCase):
+    def test_users_sign_up_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse("users:sign-up"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_users_sign_in_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse("users:sign-up"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_users_sign_up_view_loads_correct_template(self):
+        response = self.client.get(reverse("users:sign-up"))
+        self.assertTemplateUsed(response, "users/sign-up.html")
+
+    def test_users_sign_in_view_loads_correct_template(self):
+        response = self.client.get(reverse("users:sign-in"))
+        self.assertTemplateUsed(response, "users/sign-in.html")
---
+++ src/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("sign-up/", views.sign_up_view, name="sign-up"),
+    path("sign-in/", views.sign_in_view, name="sign-in"),
+]
