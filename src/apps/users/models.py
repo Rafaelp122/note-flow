ccb983fd5fa774276a2562c5c78deb2c344c6919
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-09-28 15:54:13 -0300

    feat(users): add username field with validation to CustomUser model

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,5 +1,6 @@
 # users/models.py
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.validators import MaxLengthValidator
 from django.db import models
 
 
@@ -31,9 +32,13 @@ class CustomUserManager(BaseUserManager):
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[MaxLengthValidator(30)]
+    )
 
     USERNAME_FIELD = 'email'
-
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     objects = CustomUserManager()
